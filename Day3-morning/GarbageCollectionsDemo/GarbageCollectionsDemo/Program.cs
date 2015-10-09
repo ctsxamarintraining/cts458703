525d54a35b5e33c36642b64fa67958cf3a1c4554
CTS458703 <nikhilraj.ransoth@cognizant.com>
2015-10-09 15:20:00 +0530

    GarbageCollectionDemo

---
+++ Day3-morning/GarbageCollectionsDemo/GarbageCollectionsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GarbageCollectionsDemo
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			long mem1 = GC.GetTotalMemory(false);
+			Console.WriteLine(mem1);
+			int[] values = new int[500000];
+
+			values = null;
+			long mem2 = GC.GetTotalMemory(false);
+			Console.WriteLine(mem2);
+			GC.Collect();
+			long mem3 = GC.GetTotalMemory(false);
+			Console.WriteLine(mem3);
+			//GC.RemoveMemoryPressure (mem3);
+
+		}
+	}
+}
