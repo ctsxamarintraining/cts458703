2936fd8062392ed1af68de733f4b95165b643324
CTS458703 <nikhilraj.ransoth@cognizant.com>
2015-10-12 12:53:10 +0530

    FileProb3

---
+++ Day2-Afternoon/FilesProb3/FilesProb3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FilesProb3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			FileStream stream = File.OpenRead("Yourfile.txt");
+			byte[] fileBytes= new byte[500];
+
+			stream.Read(fileBytes, 0, 500);
+			stream.Close();
+			//Begins the process of writing the byte array back to a file
+
+			using (Stream file = File.OpenWrite("Yourfile1.txt"))
+			{
+				file.Write(fileBytes, 0, fileBytes.Length);
+			}
+
+
+		}
+	}
+}
