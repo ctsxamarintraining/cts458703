f585105916ebe606b2db77dcd35069bda2e1b776
CTS458703 <nikhilraj.ransoth@cognizant.com>
2015-10-12 10:50:42 +0530

    FileReverse

---
+++ Day2-Afternoon/FileReverse/FileReverse/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+namespace FileReverse
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			FileStream fs1 = new FileStream("Yourfile.txt", FileMode.OpenOrCreate, FileAccess.Write);
+			StreamWriter writer = new StreamWriter(fs1);
+			writer.Write("Hello Welcome Sharath and Sajid");
+			writer.Close();
+
+
+			TextReader tr = new StreamReader("Yourfile.txt");
+
+			// read a line of text
+			//Console.WriteLine( tr.ReadLine());
+		
+			string WholeFileString=tr.ReadToEnd();
+			char []strArray = WholeFileString.ToCharArray();
+			Array.Reverse(strArray);
+			//again convert to string
+			string newStr = new string(strArray);
+			Console.WriteLine (newStr);
+			// close the stream
+			tr.Close();
+
+
+			FileStream fs2= new FileStream("Yourfile1.txt", FileMode.OpenOrCreate, FileAccess.Write);
+			StreamWriter writer1 = new StreamWriter(fs2);
+			writer1.Write(newStr);
+			writer1.Close();
+
+
+
+		}
+	}
+}
