5c7ed3ef73cad3bc2e38cf7b4fdb3e97bdc3d29d
CTS458703 <nikhilraj.ransoth@cognizant.com>
2015-10-12 14:06:02 +0530

    JsonSerialization

---
+++ Day2-Afternoon/JSONSerialization/JSONSerialization/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JSONSerialization
+{
+	public class Product
+	{
+		public string Name;
+		public DateTime ExpiryDate;
+		public double Price;
+		public string color;
+		public string[] Sizes= new string[3];
+
+		public Product ()
+		{
+		}
+	}
+}
+
---
+++ Day2-Afternoon/JSONSerialization/JSONSerialization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace JSONSerialization
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Product product = new Product();
+
+			product.Name = "Apple";
+			product.ExpiryDate = new DateTime(2008, 12, 28);
+			product.Price = 3.99;
+			product.Sizes = new string[] { "Small", "Medium", "Large" };
+			product.color="Red";
+
+			string output = JsonConvert.SerializeObject(product);
+			Console.Write (output);
+		}
+	}
+}
