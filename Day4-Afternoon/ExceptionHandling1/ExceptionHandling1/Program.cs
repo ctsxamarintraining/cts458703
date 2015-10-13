aeb9b5e26bbae889e1be37651b867a02ecfe4c84
CTS458703 <nikhilraj.ransoth@cognizant.com>
2015-10-13 10:58:36 +0530

    Exceptionhandling

---
+++ Day4-Afternoon/ExceptionHandling1/ExceptionHandling1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ExceptionHandling1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			try{
+				Console.Write("Enter the number");
+				string number = Console.ReadLine ();
+				int m = Convert.ToInt32(number);
+
+				double x = Math.Sqrt (m);
+				Console.Write(x);
+			}
+			catch(ArgumentException){
+
+				Console.Write ("Invalid  Number");
+			}
+			catch(FormatException){
+				
+				Console.Write ("Invalid  Number");
+
+			}
+
+			catch(NotFiniteNumberException) {
+				Console.Write ("Invalid  Number");
+			}
+
+		}
+	}
+}
---
+++ Day4-Afternoon/ExceptionHandling2/ExceptionHandling2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			try{
+				Console.WriteLine("Enter the file name");
+				string filename=Console.ReadLine();
+
+				Console.WriteLine ("The full file path is {0}", Path.GetFullPath (filename));
+				string WholeFileString=System.IO.File.ReadAllText(filename);
+			    Console.Write (WholeFileString);
+				}
+
+			catch(FileNotFoundException){
+
+				Console.WriteLine ("File does not exists, try filename:- Demo.txt");
+			}
+		}
+	}
+}
