15a47e08113404f57e76bfdea91264fc2a6ab35c
CTS458703 <nikhilraj.ransoth@cognizant.com>
2015-10-14 10:48:50 +0530

    Extension

---
+++ Day5-Morning/Extension/Extension/ExtensionMethod.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+
+namespace Extension
+{
+	public static class ExtensionMethod
+	{
+		public static int GetNoOfWords (this string input)
+		{
+			int c = 1;
+			for (int i = 1; i < input.Length; i++) {
+				if (char.IsWhiteSpace (input [i - 1]) == true) {
+					if (char.IsLetterOrDigit (input [i]) == true ||
+					    char.IsPunctuation (input [i])) {
+						c++;
+					}
+				}
+			}
+			return c;
+		}
+
+		public  static int GetNoOfCharacters (this string input)
+		{
+			int c = 1;
+			for (int i = 1; i < input.Length; i++) {
+				if (char.IsWhiteSpace(input[i])) {
+					
+				} else {
+					c++;
+				}
+			}
+			return c;
+		}
+			
+
+	}
+
+
+}
+
---
+++ Day5-Morning/Extension/Extension/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace Extension
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string str="hiiii i am mad mad guy";
+			int countOfWords = str.GetNoOfWords ();
+
+		
+			Console.WriteLine ("No Of words :{0}", countOfWords);
+			int countOfCharacters = str.GetNoOfCharacters ();
+			Console.WriteLine ("No Of characters :{0}", countOfCharacters);
+
+
+			string str1="hiiii i ";
+			int count1= str1.GetNoOfWords ();
+			Console.WriteLine ("No Of words :{0}", count1);
+
+		}
+	}
+}
