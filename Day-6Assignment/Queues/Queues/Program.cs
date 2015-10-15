a0f597cd8cd6b790b27cc2f210dc29c345e649bb
CTS458703 <nikhilraj.ransoth@cognizant.com>
2015-10-15 15:21:34 +0530

    Stacks&Queues

---
+++ Day-6Assignment/Queues/Queues/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Queues
+{
+	class MainClass
+	{
+		int[] array = new int[6];
+		int count;
+
+		int size(int[] array){
+			return array.Length;
+
+		}
+
+
+
+		void deQueue()
+		{
+
+			for (int i=0;i< array.Length-1
+				;i++) {
+				
+				array [i] = array [i + 1];
+
+			}
+			Array.Resize (ref array, count - 1);
+			
+		}
+		void enQueue(int element){
+
+			Array.Resize (ref array, count + 1);
+			array [count++] = element;
+
+		}
+		public static void Main (string[] args)
+		{
+			MainClass main = new MainClass ();
+			main.array [0] = 1;
+			main.array [1] = 2;
+			main.array [2] = 3;
+			main.array [3] = 4;
+			main.array [4] = 5;
+			main.array [5] = 6;
+			main.count = main.size (main.array);
+			main.enQueue (3);
+			main.deQueue ();
+			Console.WriteLine ("Elements after performing operations");
+			foreach (int j in main.array) {
+
+				Console.WriteLine (j);
+			}
+
+		}
+	}
+}
---
+++ Day-6Assignment/StacksQueues/StacksQueues/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Stack
+{
+	class MainClass
+	{
+		int[] array = new int[5];
+		int count=0;
+
+		int size(int[] arr){
+
+			return arr.Length;
+
+		}
+
+		void push(int element)
+		{
+			Array.Resize (ref array, count+1);
+			array [count++] = element;
+
+
+		}
+		void pop()
+		{
+			Array.Resize (ref array, count - 1);
+		}
+
+		public static void Main (string[] args)
+		{
+			MainClass main = new MainClass ();
+			main.array [0] = 1;
+			main.array [1] = 2;
+			main.array [2] = 3;
+			main.array [3] = 4;
+			main.array [4] = 5;
+			main.count = main.size (main.array);
+			main.push (6);
+			main.push (7);
+			main.pop ();
+			//Console.Write (main.array.ToString());
+			Console.WriteLine("Elements after performing operations");
+			foreach (int i in main.array)
+				Console.WriteLine (i);
+		}
+
+
+
+	}
+}
